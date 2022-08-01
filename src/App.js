10c5c8d977e7ecbe7f590502e23c88e44a720a2d
turnertop <turnerfool@pm.me>
2022-07-31 20:40:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import LoadingScreen from './assets/LoadingScreen.gif';
+
+// Gurashark loading screen for you web-app/website
+// Function UseState Demonstration
+
+class LoadingScreen2 extends React.Component {
+  render() {
+  return (
+    <html style= {{
+    backgroundColor: "#C7DCF9",
+    width: "100vw",
+    minHeight: "100vh",
+    height: "100%",
+    margin: "0px",
+    padding: "0px",
+    }}>
+    <body style={{
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    margin: "0px",
+    padding: "0px",
+    alignItems: "center",
+    }}>
+    <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    }}>
+    <img src={LoadingScreen} alt="loading screen"
+   style={{
+     position: "fixed",
+     top: "50%",
+     left: "50%",
+     transform: "translate(-50%, -50%)",
+   }}
+      />
+    </div>
+</body>
+    </html>
+  )
+
+  }
+
+}
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000)
+  }, [])
   return (
-    <div className="App">
+    <>
+    {loading === false ? (
+    <div className="App" style={{
+    }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +75,11 @@ function App() {
         </a>
       </header>
     </div>
+      ) : (
+        <LoadingScreen2 />
+      )}
+    </>
+
   );
 }
 
